5c2c2e5550285e25805343cc33c446602ade5884
Panchana Madara <panchanamadara16314@gmail.com>
2023-01-12 20:38:53 +0530

    Server creaate

--- client/main.js
+++ client/main.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:3000", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
